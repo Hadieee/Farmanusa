a729106bf5a1257f000923480f21a40f27aaf507
Hadie Pratama Tulili <89201034+Hadieee@users.noreply.github.com>
2022-11-02 15:12:04 +0800

    Add files via upload

---
+++ baru.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="baru.css">
+</head>
+
+<body>
+    <header class="kepala">
+        <a href="" class="logo">PHP</a>
+        <nav class="navbar">
+            <a href=""><i class="fas fa-home"></i> Home</a>
+            <a href=""><i class="fa-solid fa-pills"></i> Obat</a>
+            <a href=""><i class="fas fa-user-md"></i> Apoteker</a>
+            <a href=""><i class="fas fa-user"></i></a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars" style="display: none;"></div>
+    </header>
+
+    <section class="home" id="home">
+        <div class="image">
+            <img src="image/book-img.svg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>stay safe, stay healthy</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem sed autem vero? Magnam, est laboriosam!</p>
+            <a href="#" class="btn"> contact us <span class="fas fa-chevron-right"></span> </a>
+        </div>
+    </section>
+</body>
+
+</html>
