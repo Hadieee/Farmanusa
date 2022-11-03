3bc6ba62a87433478dbf1b15678d30bf4b90e508
YafiArya <ibnu.yafi.aw@gmail.com>
2022-11-03 14:20:04 +0800

    Create login-page.php

---
+++ login-page.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="baru.css">
+</head>
+
+<body>
+    <header class="kepala">
+        <a href="" class="logo">PHP</a>
+        <nav class="navbar">
+            <a href=""><i class="fas fa-home"></i> Home</a>
+            <a href=""><i class="fa-solid fa-pills"></i> Obat</a>
+            <a href=""><i class="fas fa-user-md"></i> Apoteker</a>
+            <a href=""><i class="fas fa-user"></i></a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars" style="display: none;"></div>
+    </header>
+
+
+    
+    <section class="home">
+        <table align="center">
+            <tr>
+                <td align="center">Username: </td>
+                <td align="center"><input type="text" size="20" maxlength="15" name="username"></td>
+            </tr>
+            <tr>
+                <td align="center">Password: </td>
+                <td align="center"><input size=\"20\"
+                   type="password" size="20"
+                   maxlength="15" name="password"></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"><input type="submit"
+                    value="Login"> </td>
+            </tr>
+        </table>
+    </section>
+</body>
+
+</html>
