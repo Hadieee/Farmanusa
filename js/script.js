fbde59e9f18ac47b5980675379366cdbffe866e8
Hadieee <hadiepratama15@gmail.com>
2022-11-06 14:41:39 +0800

    tambahan toggle

--- js/script.js
+++ js/script.js
@@ -33,16 +33,31 @@ if(currentTheme === "light"){
 
 let toggle = document.querySelector('#toggle');
 
+if(currentTheme === "light") {
+    toggle.checked = false;
+    img_light.style.display = "block";
+    img_dark.style.display = "none";
+}
+
+else {
+    toggle.checked = true;
+    img_light.style.display = "none";
+    img_dark.style.display = "block";
+}
+
 toggle.onclick = function(){
     var currentTheme = document.documentElement.getAttribute('data-theme');
     var targetTheme = "light";
+
     img_light.style.display = "block";
     img_dark.style.display = "none";
+
     if (currentTheme === "light") {
         targetTheme = "dark";
         img_light.style.display = "none";
         img_dark.style.display = "block";
     }
+
     document.documentElement.setAttribute('data-theme', targetTheme);
     localStorage.setItem('theme', targetTheme);
 }
