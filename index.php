6b74d2f44a048b19ccdec5f7f4716079189ad672
Hadieee <hadiepratama15@gmail.com>
2022-11-06 21:49:53 +0800

    modified:   bikin footer 	modified:   atur css footer

--- index.php
+++ index.php
@@ -12,11 +12,12 @@
 
 <body>
 
-    <!-- home section starts  -->
-
     <?php
     include 'navbar.php';
     ?>
+
+    <!-- home section starts  -->
+
     <section class='badan'>
         <div class="home" id="home">
             <div class="image">
@@ -65,6 +66,47 @@
     </section>
 
     <!-- about section ends -->
+
+    <!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#about"> <i class="fas fa-chevron-right"></i> about us</a>
+        </div>
+
+        <div class="box">
+            <h3>our services</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> obat </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> apoteker </a>
+            <a onclick="document.getElementById('popup').style.display = 'block';"> <i class="fas fa-chevron-right"></i> login </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> farmanusa@gmail.com </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Samarinda, indonesia - 141122 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Farmanusa's Team Website Development</span> | all rights NOT reserved </div>
+
+</section>
+
+    <!-- footer section ends  -->
 
     <!-- pop up login starts  -->
 
