76207c5251119bc349e35e776ba1726dad07010a
Hadieee <hadiepratama15@gmail.com>
2022-11-06 23:52:40 +0800

    new file:   image/heart-health-48.png 	bikin browser tab icon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome To Farmanusa</title>
+    <link rel="browser tab icon" href="./image/heart-health-48.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="css/index.css">
 </head>
