a24c91b7f87000007af63793c6e623ccc023968a
Hadieee <hadiepratama15@gmail.com>
2022-11-09 16:36:45 +0800

    modified:   apoteker.php

--- apoteker.php
+++ apoteker.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Medicina</title>
+    <title>Apoteker</title>
     <link rel="browser tab icon" href="./image/heart-health-48.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="css/index.css">
