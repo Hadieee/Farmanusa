86a281adca321cf3426c73cce1dda1585e1aa353
Hadieee <hadiepratama15@gmail.com>
2022-11-12 16:53:39 +0800

    Ngebaikin tampilan admin apoteker

--- admin/apoteker.php
+++ admin/apoteker.php
@@ -1,24 +1,23 @@
 <?php
-    require('../db-connect.php');
-    session_start();
-    if(isset($_SESSION['tipe_akun'])){
-        if($_SESSION['tipe_akun'] != 'admin'){
-            echo "<script>
+require('../db-connect.php');
+session_start();
+if (isset($_SESSION['tipe_akun'])) {
+    if ($_SESSION['tipe_akun'] != 'admin') {
+        echo "<script>
                 alert('Kamu Bukan Admin Woi');
                 document.location.href = '../';
             </script>";
-        }
     }
-    if(!isset($_POST['Cari'])){
-        $result = mysqli_query($db, "SELECT * FROM user WHERE tipe_akun = 'apoteker'");
-    }
-    else{
-        $cari = $_POST['Search'];
-        $result = mysqli_query($db, "SELECT * FROM user WHERE username LIKE '%$cari%' AND tipe_akun = 'apoteker'");
-    }
-    while($row = mysqli_fetch_assoc($result)){
-        $apoteker[] = $row;
-    }
+}
+if (!isset($_POST['Cari'])) {
+    $result = mysqli_query($db, "SELECT * FROM user WHERE tipe_akun = 'apoteker'");
+} else {
+    $cari = $_POST['Search'];
+    $result = mysqli_query($db, "SELECT * FROM user WHERE username LIKE '%$cari%' AND tipe_akun = 'apoteker'");
+}
+while ($row = mysqli_fetch_assoc($result)) {
+    $apoteker[] = $row;
+}
 ?>
 
 <!DOCTYPE html>
@@ -45,8 +44,8 @@
     <section class="apoteker" id="apoteker">
 
         <h1 class="heading">apoteker</h1>
-        <?php 
-            if(isset($apoteker) or isset($_POST['Cari'])){
+        <?php
+        if (isset($apoteker) or isset($_POST['Cari'])) {
         ?>
             <div style="display: flex; flex-direction:row; justify-content:space-around; align-items:center; flex-wrap: wrap;  ">
                 <button class="btn" style="padding:11.2px; margin-top: 0; display:flex; align-items:center;" onclick="document.querySelector('.popup').style.display = 'block'">+ Tambah Apoteker</button>
@@ -56,47 +55,67 @@
                 </form>
             </div>
         <?php
-            }
+        }
         ?>
         <div class="box-container">
             <?php
-            if(isset($apoteker)){
-                foreach ($apoteker as $staff):
+            if (isset($apoteker)) {
+                foreach ($apoteker as $staff) :
             ?>
-                <div class="box">
-                    <img src="../image/blog-2.jpg" alt="">
-                    <h1><?php echo $staff['username'] ?></h1>
-                    <span>expert fharmachist</span>
+                    <div class="box">
+                        <div class="image">
+                            <img src="../image/blog-2.jpg" alt="">
+                            <div class="icons">
+                                <a href=<?php echo "crud-apoteker/update-apoteker.php?id=" . $staff["email"] ?> class="fas fa-pencil"></a>
+                                <a href=<?php echo "crud-apoteker/hapus-apoteker.php?id=" . $staff["email"] ?> class="fas fa-trash"></a>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <h3><?php echo $staff['username'] ?></h3>
+                            <span>expert fharmachist</span>
+                        </div>
+                    </div>
+
+                    <!-- <div class="box">
+                <div class="image">
+                    <img src="../image/obat-1.jpg" alt="">
                     <div class="icons">
-                        <a href=<?php echo "crud-apoteker/update-apoteker.php?id=".$staff["email"]?> class="fas fa-pencil"></a>
-                        <a href=<?php echo "crud-apoteker/hapus-apoteker.php?id=".$staff["email"]?> class="fas fa-trash"></a>
+                        <a href=<?php echo "crud-obat/update-obat.php?id=" . $obt["id_obat"] ?> class="fas fa-pencil"></a>
+                        <a href=<?php echo "crud-obat/hapus-obat.php?id=" . $obt["id_obat"] ?> class="fas fa-trash"></a>
                     </div>
                 </div>
+                <div class="content">
+                    <h3><?php echo $obt['nama_obat']; ?></h3>
+                    <div class="price">Rp.<?php echo $obt['harga_obat']; ?></div>
+                </div> -->
+
+
+
+                <?php
+                endforeach;
+            } else {
+                if (!isset($_POST['Cari'])) {
+                ?>
+                    <div class="kosong">
+                        <h3>Mohon Maaf</h3>
+                        <p>
+                            Belum Ada Apoteker yang
+                            Bekerja di Farmanusa
+                        </p>
+                    </div>
+                <?php
+                } else {
+                ?>
+                    <div class="kosong">
+                        <h3>Mohon Maaf</h3>
+                        <p>
+                            Apoteker yang anda cari tidak
+                            ada di daftar apoteker kami
+                        </p>
+                    </div>
             <?php
-                endforeach;}
-                else{
-                    if(!isset($_POST['Cari'])){
-                        ?>
-                        <div class="kosong">
-                            <h3>Mohon Maaf</h3>
-                            <p>
-                                Belum Ada Apoteker yang 
-                                Bekerja di Farmanusa
-                            </p> 
-                        </div>
-                        <?php
-                            } else {
-                        ?>
-                        <div class="kosong">
-                            <h3>Mohon Maaf</h3>
-                            <p>
-                                Apoteker yang anda cari tidak 
-                                ada di daftar apoteker kami
-                            </p> 
-                        </div>
-                        <?php
-                            }
-                            }
+                }
+            }
             ?>
         </div>
     </section>
@@ -107,28 +126,36 @@
             <table align="center">
                 <tr>
                     <td align="right">Email</td>
-                    <td> <center>:</center></td>
+                    <td>
+                        <center>:</center>
+                    </td>
                     <td align="left"><input type="text" name="email" required></td>
                 </tr>
                 <tr>
                     <td align="right">Username</td>
-                    <td> <center>:</center></td>
+                    <td>
+                        <center>:</center>
+                    </td>
                     <td align="left"><input type="text" name="username" required></td>
                 </tr>
                 <tr>
                     <td align="right">Password</td>
-                    <td> <center>:</center></td>
+                    <td>
+                        <center>:</center>
+                    </td>
                     <td align="left"><input type="password" name="password" required></td>
                 </tr>
                 <tr>
                     <td align="right">Konfirmasi Password</td>
-                    <td> <center>:</center></td>
+                    <td>
+                        <center>:</center>
+                    </td>
                     <td align="left"><input type="password" name="password_konf" required></td>
                 </tr>
                 <tr>
                     <td align="center" colspan="3" style="padding-top: 10px;">
-                            <button type="submit" class="btn"> Tambah </button>
-                            <button class="btn" onclick="document.querySelector('.popup').style.display = 'none'"> Batalkan </button>
+                        <button type="submit" class="btn"> Tambah </button>
+                        <button class="btn" onclick="document.querySelector('.popup').style.display = 'none'"> Batalkan </button>
                     </td>
                 </tr>
             </table>
